2a9ca01a94d536af69a67b2ff31d22c967fa4eee
Samuel Vaillant <s.vaillant@creads.org>
2015-04-27 16:53:42 +0200

    rewrite main directive with es6

--- src/alert-banner.directive.js
+++ src/alert-banner.directive.js
@@ -1,4 +1,4 @@
-(function() {
+(() => {
 
   'use strict';
 
@@ -9,8 +9,73 @@
    * @param  {$timeout}    $timeout
    * @param  {$rootScope}  $rootScope
    * @param  {AlertBanner} AlertBanner
+   * @ngInject
    */
-  function AlertBannerDirective(ALERT_BANNER, $timeout, $rootScope, AlertBanner) {
+  class AlertBannerDirective {
+
+    constructor(ALERT_BANNER, $timeout, $rootScope, AlertBanner) {
+      this.restrict = 'E';
+      this.templateUrl = 'alert-banner.template.html';
+
+      this._ALERT_BANNER = ALERT_BANNER;
+      this._timeout = $timeout;
+      this._rootScope = $rootScope;
+      this._AlertBanner = AlertBanner;
+
+      this._options = this._AlertBanner.getDefaultOptions();
+      this.queue = [];
+    }
+
+    /**
+     * Callback for event alert:publish
+     * @param  {event}  event
+     * @param  {object} data
+     * @return {void}
+     */
+    onMessage(event, data) {
+      this.clearQueue();
+
+      angular.copy(this._options, this._$scope.alert);
+      angular.extend(this._$scope.alert, data);
+
+      this._el.querySelector('.' + this._AlertBanner.getClassName()).classList.add('active');
+
+      this.queue.push(this._timeout(() => {
+        this._$scope.alert.onOpen();
+      }, this._AlertBanner.getAnimationDuration()));
+
+      if (this._$scope.alert.autoClose) {
+        this.queue.push(this._timeout(() => {
+          this.close();
+        }, this._$scope.alert.timeCollapse));
+      }
+    }
+
+    /**
+     * Close alert message
+     * @return {void}
+     */
+    close() {
+      if (this._el.querySelector('.' + this._AlertBanner.getClassName()).classList.contains('active')) {
+        this.clearQueue();
+        this._el.querySelector('.' + this._AlertBanner.getClassName()).classList.remove('active');
+        this.queue.push(this._timeout(() => {
+          this._$scope.alert.onClose();
+          angular.copy(this._options, this._$scope.alert);
+        }, this._AlertBanner.getAnimationDuration()));
+      }
+    }
+
+    /**
+     * Clear queue for alert timer
+     * @return {void}
+     */
+    clearQueue() {
+      this.queue.forEach((promise) => {
+        this._timeout.cancel(promise);
+      });
+      this.queue = [];
+    }
 
     /**
      * @name   link
@@ -18,86 +83,21 @@
      * @param  {$scope}   $scope
      * @param  {$element} $el
      */
-    function link($scope, $el) {
-
-      var _options = AlertBanner.getDefaultOptions();
-      var queue = [];
+    link($scope, $el) {
+      this._$scope = $scope;
+      this._el = $el[0];
 
       $scope.alert = {};
-      $scope.className = AlertBanner.getClassName();
+      $scope.className = this._AlertBanner.getClassName();
 
-      $scope.close = close;
+      $scope.close = () => { this.close(); };
 
-      $scope.$on(ALERT_BANNER.EVENTS.PREFIX + ALERT_BANNER.EVENTS.TYPES.PUBLISH, onMessage);
-
-      /**
-       * Close alert message
-       * @return {void}
-       */
-      function close() {
-        if ($el[0].querySelector('.' + AlertBanner.getClassName()).classList.contains('active')) {
-          clearQueue();
-          $el[0].querySelector('.' + AlertBanner.getClassName()).classList.remove('active');
-          queue.push($timeout(function() {
-            $scope.alert.onClose();
-            angular.copy(_options, $scope.alert);
-          }, AlertBanner.getAnimationDuration()));
-        }
-      }
-
-      /**
-       * Callback for event alert:publish
-       * @param  {event}  event
-       * @param  {object} data
-       * @return {void}
-       */
-      function onMessage(event, data) {
-        clearQueue();
-
-        angular.copy(_options, $scope.alert);
-        angular.extend($scope.alert, data);
-
-        $el[0].querySelector('.' + AlertBanner.getClassName()).classList.add('active');
-
-        queue.push($timeout(function() {
-          $scope.alert.onOpen();
-        }, AlertBanner.getAnimationDuration()));
-
-        if ($scope.alert.autoClose) {
-          queue.push($timeout(function() {
-            close();
-          }, $scope.alert.timeCollapse));
-        }
-      }
-
-      /**
-       * Clear queue for alert timer
-       * @return {void}
-       */
-      function clearQueue() {
-        queue.forEach(function(promise) {
-          $timeout.cancel(promise);
-        });
-        queue = [];
-      }
+      $scope.$on(this._ALERT_BANNER.EVENTS.PREFIX + this._ALERT_BANNER.EVENTS.TYPES.PUBLISH, (event, data) => { this.onMessage(event, data); });
     }
-
-    return {
-      restrict: 'E',
-      templateUrl: 'alert-banner.template.html',
-      link: link
-    };
   }
 
-  angular
-    .module('angular-alert-banner')
-    .directive('alertBanner', [
-      'ALERT_BANNER',
-      '$timeout',
-      '$rootScope',
-      'AlertBanner',
-      AlertBannerDirective
-    ])
+  register('angular-alert-banner')
+    .directive('alertBanner', AlertBannerDirective)
   ;
 
 }());
